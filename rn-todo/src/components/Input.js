4f2c63106db27cfa092da80491de2fb176b17035
hyeongukSeo <shw8907@google.com>
2025-01-13 17:26:37 +0900

    to-do 키보드 세팅... 기본값을 어떻게 주나..

---
+++ rn-todo/src/components/Input.js
@@ -0,0 +1,48 @@
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+import PropTypes from 'prop-types';
+
+export const KeyboardTypes = {
+  DEFAULT: 'default',
+  EMAIL: 'email-address',
+};
+
+const Input = ({ title, placeholder, keyboardType }) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>{title}</Text>
+      <TextInput
+        style={styles.input}
+        placeholder={placeholder ?? title}
+        placeholderTextColor={'#a3a3a3'}
+        autoCapitalize="none"
+        autoCorrect={false}
+        keyboardType={keyboardType}
+      />
+    </View>
+  );
+};
+
+Input.propTypes = {
+  title: PropTypes.string.isRequired,
+  placeholder: PropTypes.string,
+  keyboardType: PropTypes.oneOf(Object.values(KeyboardTypes)),
+};
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    paddingHorizontal: 20,
+    marginVertical: 10,
+  },
+  title: {
+    marginBottom: 4,
+  },
+  input: {
+    borderWidth: 1,
+    borderRadius: 8,
+    paddingHorizontal: 10,
+    height: 42,
+  },
+});
+
+export default Input;
